4fcddb2f8fbd25e9113e04815fcdaac499371504
Venkatesh <venkateshparuchuri20@gmail.com>
2026-04-28 14:10:53 +0530

    uc6 done

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,34 +1,18 @@
 public class TicTacToe {
 
-    static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
-    };
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
 
-        // Test cases
-        System.out.println(isValidMove(1, 1)); // true
-        System.out.println(isValidMove(3, 0)); // false (out of bounds)
+        // Sample move
+        placeMove(0, 0, 'X');
 
-        board[0][0] = 'X';
-        System.out.println(isValidMove(0, 0)); // false (already filled)
+        // Print updated cell
+        System.out.println(board[0][0]);
     }
 
-    // Checks if move is valid
-    static boolean isValidMove(int row, int col) {
-
-        // Check boundaries
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        // Check if cell is empty
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
+    // UC6: Place move (assumes move already validated)
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 }
